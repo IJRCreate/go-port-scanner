ddd455efe792f9fabaf548651ac5f21efbed9525
Isaac Robinson <69752141+IJRCreate@users.noreply.github.com>
2023-12-10 02:34:13 +0000

    Added portScan function to check multiple ports.

--- scan.go
+++ scan.go
@@ -3,25 +3,59 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net"
 )
 
 var targetHost string
 var targetPort string
 
+// TODO allow multiple ports to be scanner
 func init() {
 	flag.StringVar(&targetHost, "H", "", "specify target host")
 	flag.StringVar(&targetPort, "p", "", "specify target port")
 }
 
+// TODO call portScan and not connectionScan
 func main() {
 	flag.Parse()
 	if targetHost == "" || targetPort == "" {
 		flag.Usage()
 	}
-	scan(flag.Args())
+	fmt.Println(connectionScan(targetHost, targetPort))
 }
 
-func scan(args []string) string {
-	return fmt.Sprintf("%v", args)
-	//return fmt.Sprintf("Target Host: %s\nTargetPort\n: %d", targetHost, targetPort)
+// TODO move hostname & address checks from connectionScan to portScan
+func portScan(host string, ports []string) []string {
+	results := make([]string, len(ports))
+	for i, port := range ports {
+		results[i] = connectionScan(host, port)
+	}
+	return results
+}
+
+func connectionScan(host string, port string) string {
+
+	var targetHost string
+	ipAddress, err := net.LookupHost(host)
+	if err != nil {
+		// TODO iterate through all returned ip Addresses
+		targetHost = ipAddress[0]
+	} else {
+		fmt.Printf("Cannot resolve '%s': Unknown host\n", host)
+		targetHost = host
+	}
+	targetPort := port
+	target := fmt.Sprintf("%s:%s", targetHost, targetPort)
+
+	_, err = net.Dial("tcp", target)
+	if err != nil {
+		return "TCP Closed"
+	} else {
+		if err != nil {
+			log.Fatal(err)
+		}
+		return "TCP Open"
+	}
+
 }
--- scan_test.go
+++ scan_test.go
@@ -1,21 +1,98 @@
 package main
 
-import "testing"
+import (
+	"log"
+	"os"
+	"os/exec"
+	"slices"
+	"strings"
+	"testing"
+	"time"
+)
 
 type testData struct {
-	input    []string
-	expected string
+	host     string
+	ports    []string
+	expected []string
 }
 
-func Test(t *testing.T) {
+var passingTests = []testData{
+	{host: "127.0.0.1", ports: []string{"8000"}, expected: []string{"TCP Open"}},
+}
 
-	tests := []testData{
-		{input: []string{"127.0.0.1", "80"}, expected: "[127.0.0.1 80]"},
+var failingTests = []testData{
+	{host: "127.0.0.1", ports: []string{"8001"}, expected: []string{"TCP Closed"}},
+}
+
+func openPort(port string) *exec.Cmd {
+	// TODO Use commented commands to emulate banners for later grabbing
+	// echo := exec.Command("echo", test.expected)
+	netcat := exec.Command("nc", "-lvp", port)
+	//netcat.Stdin, _ = echo.StdoutPipe()
+	err := netcat.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Required to allow netcat to start in time
+	time.Sleep(5 * time.Millisecond)
+	/* err = echo.Run()
+	if err != nil {
+		log.Fatal(err)
+	} */
+	return netcat
+}
+
+func closePort(netcat *exec.Cmd) {
+	netcat.Process.Signal(os.Interrupt)
+}
+
+func TestConnectionScan(t *testing.T) {
+
+	for _, test := range passingTests {
+
+		listener := openPort(test.ports[0])
+
+		actual := strings.TrimSpace(connectionScan(test.host, test.ports[0]))
+		if actual != test.expected[0] {
+			t.Errorf("Expected: %s; Actual: %s", test.expected, actual)
+		}
+
+		closePort(listener)
 	}
 
-	for _, test := range tests {
-		actual := scan(test.input)
-		if actual != test.expected {
+	for _, test := range failingTests {
+		actual := strings.TrimSpace(connectionScan(test.host, test.ports[0]))
+		if actual != test.expected[0] {
+			t.Errorf("Expected: %s; Actual: %s", test.expected, actual)
+		}
+	}
+
+}
+
+func TestPortScan(t *testing.T) {
+	for _, test := range passingTests {
+		var actual []string
+		listeners := make([]*exec.Cmd, len(test.ports))
+		for i, port := range test.ports {
+			listeners[i] = openPort(port)
+		}
+
+		actual = portScan(test.host, test.ports)
+
+		if !slices.Equal(actual, test.expected) {
+			t.Errorf("Expected: %s; Actual: %s", test.expected, actual)
+		}
+
+		for _, listener := range listeners {
+			closePort(listener)
+		}
+
+	}
+
+	for _, test := range failingTests {
+		var actual []string
+		actual = portScan(test.host, test.ports)
+		if !slices.Equal(actual, test.expected) {
 			t.Errorf("Expected: %s; Actual: %s", test.expected, actual)
 		}
 	}
